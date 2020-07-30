c0ca1cc7c3ad66b52726d00adca804f47b0a0c11
Sean Lynch <techniq35@gmail.com>
2020-07-30 09:16:28 -0400

    Add ClevelandDotPlot

---
+++ src/components/ClevelandDotPlot.svelte
@@ -0,0 +1,41 @@
+<script>
+  import { getContext } from 'svelte';
+
+  const { data, xGet, yGet, yScale, zGet } = getContext('LayerCake');
+
+  export let r = 5;
+
+  $: midHeight = $yScale.bandwidth() / 2;
+</script>
+
+<g class="dot-plot">
+  {#each $data as row}
+    <g class="dot-row">
+      <line
+        x1={Math.min(...$xGet(row))}
+        y1={$yGet(row) + midHeight}
+        x2={Math.max(...$xGet(row))}
+        y2={$yGet(row) + midHeight}
+      />
+      {#each $xGet(row) as circleX, i}
+        <circle
+          cx={circleX}
+          cy={$yGet(row) + midHeight}
+          {r}
+          fill={$zGet(row)}
+        />
+      {/each}
+    </g>
+  {/each}
+</g>
+
+<style>
+  line {
+    stroke-width: 1px;
+    stroke: #000;
+  }
+  circle {
+    stroke: #000;
+    stroke-width: 1px;
+  }
+</style>
